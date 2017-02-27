b1637c34548ef0614aee213f18fc68c79276fc4e
kbop543 <kstephen852@hotmail.com>
2017-02-27 14:16:07 -0500

    added html

--- index.js
+++ index.js
@@ -3,11 +3,10 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 
-
 var app = express();
 
-app.set('view engine', 'pug');
-app.use(express.static(__dirname));
+app.use(express.static(__dirname + '/assets'));
+app.use(express.static(__dirname + '/'));
 
 
 
@@ -17,30 +16,18 @@ app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
     extended: true
 }));
 
-// Main page.
+// Main page. 
 app.get('/', function(req, res) {
-
-	res.render('index', {
-    title: 'Butterfly'
-        });
-})
-
+    res.sendfile('index.html');
+});
 
 
 //Routes
 
-/*app.get('/applicants', ta.findAll);
+/*app.get('/thingHere', butterfly.findAll); */
 
-app.post('/applicants', ta.addOne);
-
-app.delete('/applicants', ta.delOne);
-
-app.get('/courses', ta.findWithCourses);*/
 
 
 // Start the server
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-    console.log(`Our app is running on port ${ PORT }`);
-});
+app.listen(3000);
 console.log('Listening on port 3000');
